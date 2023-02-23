18f5f09792362715c2239c870dd37ba16c7088a6
Michael Dariam Sandoval Lancheros <michael.sandoval@sinco.com.co>
2023-02-23 12:21:45 -0500

    camara4

--- src/Componentes/CamaraQR.jsx
+++ src/Componentes/CamaraQR.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { QrReader } from "react-qr-reader";
 
-const result = ()=>{
-    alert("pruebas")
-}
 
 export const QRCamara = () => {
     return(
-        <QrReader
-        onResult={result}
-            legacyMode
-        />
+        <QrReader/>
     )
 }
