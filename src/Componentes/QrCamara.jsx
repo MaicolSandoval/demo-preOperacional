541aee3e39d2ed78dac65312a88df5c160ab8803
Michael Dariam Sandoval Lancheros <michael.sandoval@sinco.com.co>
2023-02-23 15:44:43 -0500

    ajustes camara trasera 2

--- src/Componentes/QrCamara.jsx
+++ src/Componentes/QrCamara.jsx
@@ -1,17 +1,74 @@
-import React from "react";
-// import Webcam from "react-webcam";
-import { QrReader } from "react-qr-reader";
+// import React from "react";
+// // import Webcam from "react-webcam";
+// import { QrReader } from "react-qr-reader";
 
 
-export const QRcamara = () => {
+// export const QRcamara = () => {
+//     return(
+//         // <Webcam/>
+//         <QrReader
 
-    return(
-        // <Webcam/>
-        <QrReader
+//          facingMode= { {exact: "environment"} }
 
-         facingMode= { {exact: "environment"} }
+//         />
+//     )
+// }
 
-        />
+import React, { Component } from 'react'
+import { QrReader } from 'react-qr-reader'
+
+// import QrReader from 'react-qr-scanner'
+ 
+class Test extends Component {
+  state = {
+  result: '',
+  facingMode : 'user',
+  legacyMode: false,
+
+  }
+
+  handleScan(data){
+    this.setState({
+      result: data,
+    })
+  }
+  handleError(err){
+    this.setState(
+      {legacyMode: true}
     )
+  }
+
+choiceButton= () =>{
+        if(this.state.facingMode === "environment"){
+            this.setState({
+                facingMode: "user"
+            })
+        } else {
+            this.setState({
+                facingMode: "environment"
+            })
+        }
+      }
+openImageDialog() { this.ref.qrReader1.openImageDialog()  }
+
+
+  render(){
+    const previewStyle = {
+      height: 240,
+      width: 320,
+    }
+ 
+    return(
+      <div>
+        <QrReader
+          delay={this.state.delay}
+          style={previewStyle}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          />
+      </div>
+    )
+  }
 }
 
+export default Test
