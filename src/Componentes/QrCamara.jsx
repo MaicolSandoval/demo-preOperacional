80056e429065cc40f4bca8411cac832345a0cd23
Michael Dariam Sandoval Lancheros <michael.sandoval@sinco.com.co>
2023-02-23 16:45:21 -0500

    ajustes camara trasera 3

--- src/Componentes/QrCamara.jsx
+++ src/Componentes/QrCamara.jsx
@@ -1,74 +1,25 @@
-// import React from "react";
-// // import Webcam from "react-webcam";
-// import { QrReader } from "react-qr-reader";
+import React from "react";
+// import Webcam from "react-webcam";
+import { QrReader } from "react-qr-reader";
 
 
-// export const QRcamara = () => {
-//     return(
-//         // <Webcam/>
-//         <QrReader
-
-//          facingMode= { {exact: "environment"} }
-
-//         />
-//     )
-// }
-
-import React, { Component } from 'react'
-import { QrReader } from 'react-qr-reader'
-
-// import QrReader from 'react-qr-scanner'
- 
-class Test extends Component {
-  state = {
-  result: '',
-  facingMode : 'user',
-  legacyMode: false,
-
-  }
-
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
-  }
-  handleError(err){
-    this.setState(
-      {legacyMode: true}
-    )
-  }
-
-choiceButton= () =>{
-        if(this.state.facingMode === "environment"){
-            this.setState({
-                facingMode: "user"
-            })
-        } else {
-            this.setState({
-                facingMode: "environment"
-            })
-        }
-      }
-openImageDialog() { this.ref.qrReader1.openImageDialog()  }
-
-
-  render(){
-    const previewStyle = {
-      height: 240,
-      width: 320,
-    }
- 
+export const QRcamara = () => {
     return(
-      <div>
+        // <Webcam/>
+
         <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          />
-      </div>
+        key="environment"
+        constraints={{ facingMode: 'environment' }}
+        onResult={(result, error) => {
+            // stuff...
+        }}
+        />
+
+        // <QrReader
+
+        //  facingMode= { {exact: "environment"} }
+
+        // />
     )
-  }
 }
 
-export default Test
--- src/Componentes/Rutas.jsx
+++ src/Componentes/Rutas.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route } from "react-router-dom";
 import { CardConsulta } from "./CardConsulta";
 import { PrimerComponente } from "./PrimerComponente";
-import Test from "./QrCamara";
+import {QRcamara} from "./QrCamara";
 
 
 export const Rutas = () => {
@@ -11,7 +11,7 @@ export const Rutas = () => {
             <Routes>
                 <Route element={<PrimerComponente/>} path='/Sinco/V3/Maquinaria/index'></Route>
                 <Route element={<CardConsulta/>} path='/CardComponent'></Route>
-                <Route element={<Test/>} path='/QRCamara'></Route>
+                <Route element={<QRcamara/>} path='/QRCamara'></Route>
             </Routes>
         </>
     )
