604e44ad9757302b63bf9b002be4a96b365d0f6e
Michael Dariam Sandoval Lancheros <michael.sandoval@sinco.com.co>
2023-02-23 13:30:09 -0500

    ajustes pack json3

--- src/Componentes/QrCamara.jsx
+++ src/Componentes/QrCamara.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Webcam from "react-webcam";
+import QrReader from "react-qr-scanner";
 
 export const QRcamara = () => {
 
     return(
-        <Webcam/>
+        // <Webcam/>
+        <QrReader/>
     )
 }
